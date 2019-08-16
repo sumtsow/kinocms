878fe1d8336a212a944503bcec22d1b5633a9266
sumtsow <31659165+sumtsow@users.noreply.github.com>
2019-08-16 13:50:14 +0300

    Update

--- views/layouts/content.php
+++ views/layouts/content.php
@@ -37,7 +37,7 @@ use dmstr\widgets\Alert;
 
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.0
+        <b>Version</b> 1.0.1
     </div>
     <strong>Copyright &copy; <?= date('Y') ?> <a href="mailto://sumtsow@gmail.com">Dmytro Sumtsov</a>.</strong> All rights reserved.
 </footer>
